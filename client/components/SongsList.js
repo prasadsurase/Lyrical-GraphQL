bf4ad90c82f21f80a2379706965cb114e073f2b6
Prasad Surase <prasad.surase@coupa.com>
2024-05-10 15:57:15 +0530

    Added a React component to render songs list

---
+++ client/components/SongsList.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import gql from 'graphql-tag';
+import { graphql } from "react-apollo";
+
+class SongsList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return(
+        <li key={song.id}>
+          {song.title}
+        </li>
+      )
+    });
+  }
+  render() {
+    if(this.props.data.loading) {
+      return(
+        <div>Loading...</div>
+      )
+    }
+    else {
+      return (
+        <div>
+          {this.renderSongs()}
+        </div>
+      )
+    }
+  }
+}
+
+const songsQuery = gql`
+{
+  songs{
+    id
+    title
+  }
+}
+`;
+
+export default graphql(songsQuery)(SongsList);
