882443cf5fcd477157e6bafdb2e212d0ea15f985
Prasad Surase <prasad.surase@coupa.com>
2024-05-16 10:39:12 +0530

    Renders songs list

--- client/components/SongsList.js
+++ client/components/SongsList.js
@@ -6,12 +6,13 @@ class SongsList extends Component {
   renderSongs() {
     return this.props.data.songs.map(song => {
       return(
-        <li key={song.id}>
+        <li key={song.id} className="collection-item">
           {song.title}
         </li>
       )
     });
   }
+  
   render() {
     if(this.props.data.loading) {
       return(
